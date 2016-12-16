1ce7c97aebd6d1e9f8e776bfcff6ea2067f7b44c
Gerard Simpson <gardesimpson@gmail.com>
2016-12-16 22:36:50 +1000

    refining html and js

--- index.js
+++ index.js
@@ -21,21 +21,14 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length
   }
-  console.log(slides[slideIndex-1].classList);
+
   for (i = 0; i < slides.length; i++) {
-    slides[i].className = slides[i].className.replace(" active", "");
-    slides[i].className = slides[i].className.replace(" prev", "");
-    slides[i].className = slides[i].className.replace(" next", "");
-
+    slides[i].className = slides[i].className.replace(" active", "").replace(" prev", "").replace(" next", "");
   }
-
+  
   slides[slideIndex-1].className += " active";
-
   if(slideIndex < slides.length) {slides[slideIndex].className += " next";}
-
   if(slideIndex > 1) { slides[slideIndex-2].className += " prev";}
-
-
 }
 
 document.onkeydown = checkKey;
